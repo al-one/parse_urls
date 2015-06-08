c9984fcdf0795f68a1a27767bf6bc308e0fafea9
Alone <Alone@an56.net>
2015-06-08 11:07:21 +0800

    添加对mailto:、tel:、sms:等没有双斜线(//)的URL的支持

--- parse_urls.php
+++ parse_urls.php
@@ -4,14 +4,14 @@
  * @author  Alone <alone@an56.net>
  * @param   string $url 网址
  * @return  array
- * @version 0.1.20150526
+ * @version 0.1.20150608
  */
 function parse_urls($url = '')
 {
   $arr = array();
   if($url)
   {
-    $reg = '/\s*(?:(?<protocol>(?<scheme>[^:]+):)?\/\/)?((?:(?<username>(?<user>[^:@]+))(?::(?<password>(?<pass>[^@]*)))?@)?(?<host>(?<domain>[^:\/\\\?#\']+)(?::(?<port>\d+))?))?(?<pathname>\/(?<path>[^\?#]*))?(?<search>\?(?<query>[^#]*))?(?<hash>#(?<fragment>.*))?\s*/isu';
+    $reg = '/\s*(?<protocol>(?<scheme>[^:]+):)?(?<slashes>\/\/)?(?<authority>(?:(?<username>(?<user>[^:@]+))(?::(?<password>(?<pass>[^@]*)))?@)?(?<host>(?<domain>[^:\/\\\?#\']+)(?::(?<port>\d+))?))?(?<pathname>\/(?<path>[^\?#]*))?(?<search>\?(?<query>[^#]*))?(?<hash>#(?<fragment>.*))?\s*/isu';
     preg_match($reg,$url,$arr);
   }
   return $arr;
